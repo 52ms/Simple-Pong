ec1590d44dc073498c4897dccce8f29106b2f5b2
52ms <134506513+52ms@users.noreply.github.com>
2024-08-01 18:00:08 -0400

    Changed colors to standard pong colors

--- main.c
+++ main.c
@@ -111,7 +111,7 @@ int main() {
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
 
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         // Draw Left Paddle
